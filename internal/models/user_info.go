b886e015ffd32d0df9403e7830bc7fac6a4bac8e
bangiam <139111446+bangiam@users.noreply.github.com>
2023-07-12 21:23:39 +0700

    task
    task

--- internal/models/user_info.go
+++ internal/models/user_info.go
@@ -2,8 +2,8 @@ package models
 
 type UserInfo struct {
 	Id                 int       `gorm:"primarykey;column:id" json:"id" redis:"id"`
-	UserId 			   int 		 `gorm:"foreignkey;column:user_id" json:"user_id" redis:"user_id"`
-	NumberUploaded     int 		 `gorm:"column:number_uploaded" json:"number_uploaded" redis:"number_uploaded" validate:"omitempty"`
-	NumberPeerReviewed int 		 `gorm:"column:number_peer_reviewed" json:"number_peer_reviewed" redis:"number_peer_reviewed" validate:"omitempty"`
-	NumberSpecReviewed int 		 `gorm:"column:number_spec_reviewed" json:"number_spec_reviewed" redis:"number_spec_reviewed" validate:"omitempty"`
+	UserId 			   int 		 `gorm:"column:user_id" json:"user_id" redis:"user_id"`
+	NumberUploaded     int 		 `gorm:"default;column:number_uploaded" json:"number_uploaded" redis:"number_uploaded"`
+	NumberPeerReviewed int 		 `gorm:"default;column:number_peer_reviewed" json:"number_peer_reviewed" redis:"number_peer_reviewed"`
+	NumberSpecReviewed int 		 `gorm:"default;column:number_spec_reviewed" json:"number_spec_reviewed" redis:"number_spec_reviewed"`
 }

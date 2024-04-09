7523d3ed670187c84687b16eff9f06e688347035
Vu Ky <vukynpro@gmail.com>
2024-04-09 23:30:16 +0700

    feat: add/migrate entity school

---
+++ internal/school/entity/entity.go
@@ -0,0 +1,69 @@
+package entity
+
+import (
+	"InitiaRe-website/constant"
+	"InitiaRe-website/internal/article/models"
+
+	"github.com/jinzhu/copier"
+)
+
+type School struct {
+	Id         int    `gorm:"primarykey;column:id"`
+	SchoolName string `gorm:"column:school_name"`
+	Note       string `gorm:"column:note;default:(-)"`
+	StatusId   int    `gorm:"column:status_id"` // 1: Pending, 2: Rejected, 3: Verified
+
+	// Custom fields
+	StatusName string `gorm:"->;-:migration"`
+}
+
+func (a *School) TableName() string {
+	return "initiaRe_school"
+}
+
+func (a *School) Export() *models.Response {
+	obj := &models.Response{}
+	copier.Copy(obj, a) //nolint
+	return obj
+}
+
+func (a *School) ExportList(in []*School) []*models.Response {
+	objs := make([]*models.Response, 0)
+	for _, v := range in {
+		objs = append(objs, v.Export())
+	}
+	return objs
+}
+
+func (a *School) parseFromSaveRequest(req *models.SaveRequest) {
+	copier.Copy(a, req) //nolint
+}
+
+func (a *School) ParseForCreate(req *models.SaveRequest, userId int) {
+	a.parseFromSaveRequest(req)
+	a.StatusId = constant.SCHOOL_STATUS_PENDING
+}
+
+func (a *School) ParseForCreateMany(reqs []*models.SaveRequest, userId int) []*School {
+	objs := make([]*School, 0)
+	for _, v := range reqs {
+		obj := &School{}
+		obj.ParseForCreate(v, userId)
+		objs = append(objs, obj)
+	}
+	return objs
+}
+
+func (a *School) ParseForUpdate(req *models.SaveRequest, userId int) {
+	a.parseFromSaveRequest(req)
+}
+
+func (a *School) ParseForUpdateMany(reqs []*models.SaveRequest, userId int) []*School {
+	objs := make([]*School, 0)
+	for _, v := range reqs {
+		obj := &School{}
+		obj.ParseForUpdate(v, userId)
+		objs = append(objs, obj)
+	}
+	return objs
+}
